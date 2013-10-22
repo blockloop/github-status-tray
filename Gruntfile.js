5142c50d1e362f25320f88558d43b3b6c07e158d
Brett Jones <bjones@onetechnologies.net>
2013-10-22 16:00:24 -0500

    using harp

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,93 +1,22 @@
 (function(){
-    var scriptFiles = [
-        'src/coffee/**/*.coffee'
-    ];
-
     module.exports = function(grunt) {
         grunt.initConfig({
-            jshint: {
-                options: {
-                    jshintrc: '/home/brett/Dropbox/configs/dotfilex/.jshintrc'
-                },
-                all: scriptFiles
-            },
-            jade: {
-                compile: {
-                    options: {
-                        pretty: true,
-                        data: {
-                            debug: false
-                        }
-                    },
-                    files: [{
-                        expand: true,
-                        cwd: "src/jade/",
-                        src: "*.jade",
-                        dest: "app/views/",
-                        ext: ".html"
-                    }]
-                }
-
-            },
-            stylus: {
-                compile: {
-                    options:{
-                        compress: true,
-                    },
-                    files: [{
-                        expand: true,
-                        cwd: 'src/styl/',
-                        src: ['*.styl'],
-                        dest: 'app/styles',
-                        ext: '.css',
-                    }]
-                }
-            },
-            coffee: {
-                compile: {
-                    options:{
-                        join: false,
-                        sourceMap: false
-                    },
-                    files: {
-                        'app/scripts/node-main.js': 'src/coffee/node-main.coffee',
-                        'app/scripts/main.js': 'src/coffee/main.coffee',
-                        'app/scripts/site.js': [
-                            'src/coffee/**/*.coffee',
-                            '!src/coffee/node-main.coffee',
-                            '!src/coffee/main.coffee'
-                        ]
-                    }
-                }
-            },
             watch: {
                 stylus: {
-                    files: 'src/styl/*.styl',
-                    tasks: ['stylus']
+                    files: 'src/**',
+                    tasks: ['exec:harp']
                 },
-                scripts: {
-                    files : scriptFiles,
-                    tasks: ['coffee:compile','test'],
-                    options: {
-                        interrupt: true,
-                    },
-                },
-                test: {
-                    files: [
-                        'test/**/*.js'
-                    ],
-                    tasks: ['test']
-                },
-                jade: {
-                    files: 'src/jade/**/*.jade',
-                    tasks: ['jade'],
-                }
             },
             exec: {
                 test: {
-                    // command: 'node test/mocha-runner.js',
                     command: 'echo "tests not implemented"',
-                    stdout: true
+                    stdout: false,
+                    stderr: true
+                },
+                harp: {
+                    command: 'harp compile src app',
+                    stdout: false,
+                    stderr: true
                 }
             },
             notify_hooks: {
@@ -100,16 +29,10 @@
         });
 
         // Default task
-        grunt.registerTask('default','dev');
-        grunt.registerTask('dev', ['compile','watch']);
+        grunt.registerTask('default','compile');
         grunt.registerTask('test', ['exec:test']);
-        grunt.registerTask('compile', ['coffee','stylus','jade:compile'])
-        grunt.registerTask('bootstrap', ['jshint','compile'])
-        grunt.loadNpmTasks('grunt-contrib-stylus');
+        grunt.registerTask('compile', ['exec:harp'])
         grunt.loadNpmTasks('grunt-contrib-watch');
-        grunt.loadNpmTasks('grunt-contrib-jshint');
-        grunt.loadNpmTasks('grunt-contrib-jade');
-        grunt.loadNpmTasks('grunt-contrib-coffee');
         grunt.loadNpmTasks('grunt-notify');
         grunt.loadNpmTasks('grunt-exec');
 
